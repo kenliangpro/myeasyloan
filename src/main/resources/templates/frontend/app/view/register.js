2a828b81ff961fa9599c06249c078bf5ce3b351c
ken_liang <949551803@qq.com>
2017-10-02 15:11:40 +0800

    新增和修改个人贷款，公司贷款，个人借钱功能

--- src/main/resources/templates/frontend/app/view/register.js
+++ src/main/resources/templates/frontend/app/view/register.js
@@ -1,122 +1,126 @@
 define([
     "text!../tpl/userPages/register.html",
     "../util/service",
-    ],function(tpl,service) {
+], function (tpl, service) {
     var Backbone = require('backbone');
     var main = Backbone.View.extend({
         el: '#content',
-        events:{
-            "click #submit":"submit",
-            "click #send":"sendcode",
+        events: {
+            "click #submit": "submit",
+            "click #send": "sendcode",
         },
-        initialize:function () {
+        initialize: function () {
             // alert(123);
         },
 
 
-
-        sendcode: function(){
+        sendcode: function () {
             username = $('#username').val();
-            if (username=='') {
+            if (username == '') {
                 bootbox.confirm({
-                  size: "small",
-                  message: "请输入手机号码",
-                  callback: function(result){}
+                    size: "small",
+                    message: "请输入手机号码",
+                    callback: function (result) {
+                    }
                 })
 
-            }else{
-                var data={
-                    username : username,
+            } else {
+                var data = {
+                    username: username,
                 };
                 // service.sendcode(data);
 
-                service.sendcode(data).then(function(res){
-                    if(res.state==-1){
+                service.sendcode(data).then(function (res) {
+                    if (res.state == -1) {
                         bootbox.confirm({
                             size: "small",
                             message: "无效号码",
-                            callback: function(result){}
+                            callback: function (result) {
+                            }
                         })
 
-                    }else {
-                        var countdown=20;
+                    } else {
+                        var countdown = 20;
                         var obj = $("#send");
                         settime(obj);
 
                         function settime(obj) { //发送验证码倒计时
                             if (countdown == 0) {
-                                obj.attr('disabled',false);
+                                obj.attr('disabled', false);
                                 //obj.removeattr("disabled");
                                 obj.val("免费获取验证码");
                                 countdown = 20;
                                 return;
                             } else {
-                                obj.attr('disabled',true);
+                                obj.attr('disabled', true);
                                 obj.val("重新发送(" + countdown + ")");
                                 countdown--;
                             }
-                            setTimeout(function() {
-                                    settime(obj) }
-                                ,1000)
+                            setTimeout(function () {
+                                    settime(obj)
+                                }
+                                , 1000)
                         }
                     }
-
-
                 });
-                
-                
+
+
             }
         },
 
 
-        submit: function(){
+        submit: function () {
             username = $('#username').val();
             pass = $('#pass').val();
             conpass = $('#conpass').val();
             verification_code = $('#verification_code').val();
-             if(username=='' || pass=='' || conpass=='' || verification_code==''){
-                    bootbox.confirm({ 
-                      size: "small",
-                      message: "请输入完整信息", 
-                      callback: function(result){}
-                    })
-                    return false;
-                }else if (pass!=conpass) {
-                    bootbox.confirm({ 
-                      size: "small",
-                      message: "密码前后不一致，请重新输入", 
-                      callback: function(result){}
-                    })
+            if (username == '' || pass == '' || conpass == '' || verification_code == '') {
+                bootbox.confirm({
+                    size: "small",
+                    message: "请输入完整信息",
+                    callback: function (result) {
+                    }
+                })
+                return false;
+            } else if (pass != conpass) {
+                bootbox.confirm({
+                    size: "small",
+                    message: "密码前后不一致，请重新输入",
+                    callback: function (result) {
+                    }
+                })
 
-                    return false;
-                }else{ 
-                    var data = {
-                                username : username,
-                                password : pass,
-                                verification_code : verification_code,
-                            };
-                         service.userRegister(data).then(function (result) {
-                             if(result.message=='用户已存在'){
-                                 bootbox.confirm({
-                                     size: "small",
-                                     message: "用户已存在请重新注册",
-                                     callback: function(result){}
-                                 })
-                             }else if(result.message=='验证码不正确'){
-                                 bootbox.confirm({
-                                     size: "small",
-                                     message: "验证码不正确",
-                                     callback: function(result){}
-                                 })
-                             }else {
+                return false;
+            } else {
+                var data = {
+                    username: username,
+                    password: pass,
+                    verification_code: verification_code,
+                };
+                service.userRegister(data).then(function (result) {
+                    if (result.message == '用户已存在') {
+                        bootbox.confirm({
+                            size: "small",
+                            message: "用户已存在请重新注册",
+                            callback: function (result) {
+                            }
+                        })
+                    } else if (result.message == '验证码不正确') {
+                        bootbox.confirm({
+                            size: "small",
+                            message: "验证码不正确",
+                            callback: function (result) {
+                            }
+                        })
+                    } else {
 
-                                 window.location.href='#/';
-                             }
-                         });
-                         }
+                        window.location.href = '#/';
+                    }
+                });
+            }
         },
 
-        render: function(param) {
+        render: function (param) {
             $('#content').html(tpl);
         },
     });
